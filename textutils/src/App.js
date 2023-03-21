d1bc97a9d8d0ce589b3ed09d3aee6fd18886041e
Shabari <reddyshabari05@gmail.com>
2023-03-21 19:59:40 +0530

    props-1

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -5,8 +5,8 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-        <Navbar/>
-        
+        <Navbar title="TextUtils" aboutText="Description about text"/>
+
     </>
   );
 }
