734d56f0d708abbf081d065e8f5618965de27d9d
Shabari <reddyshabari05@gmail.com>
2023-03-21 10:59:03 +0530

    5th

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -16,19 +16,9 @@ function App() {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link" href="/">About</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
+        
         
       </ul>
       <form className="d-flex" role="search">
