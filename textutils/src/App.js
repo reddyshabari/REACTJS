45f12e4a35780426cc2a83896bca7ed836f59ebb
Shabari <reddyshabari05@gmail.com>
2023-03-22 22:22:23 +0530

    Changing title dynamically & adding favicons to TextUtils

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -25,11 +25,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been enabled","success");
+      document.title='TextUtils - DarkMode'
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been enabled","success");
+      document.title='TextUtils - LightMode'
     }
   }
   return (
