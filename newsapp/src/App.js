292f62298389c9014576782bcc0f318e8d966a3b
Shabari <reddyshabari05@gmail.com>
2023-03-26 12:26:46 +0530

    Understanding state in class based components

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,14 +1,16 @@
-
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  c="Shabari"
+
   render() {
     return (
       <div>
-        Hello my first class based component{this.c}
+        <NavBar/>
+        <News/>
       </div>
     )
   }
