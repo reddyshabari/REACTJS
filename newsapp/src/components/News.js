1dfb8ae6284fe9c9c326afb89e8959e440f9f95d
Shabari <reddyshabari05@gmail.com>
2023-03-26 15:31:31 +0530

    using fetch API

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,47 +1,7 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
 export class News extends Component {
-  articles=  [
-    {
-      "source": {
-        "id": "al-jazeera-english",
-        "name": "Al Jazeera English"
-      },
-      "author": "Al Jazeera",
-      "title": "Afghanistan beats Pakistan for first time in T20I cricket series",
-      "description": "Afghanistan and Pakistan met seven times in total with Pakistan always coming out on top – until the match on Friday.",
-      "url": "http://www.aljazeera.com/news/2023/3/25/afghanistan-beats-pakistan-for-first-time-in-t20i-cricket-series",
-      "urlToImage": "https://www.aljazeera.com/wp-content/uploads/2023/03/AP22308418907883.jpg?resize=1200%2C675",
-      "publishedAt": "2023-03-25T03:04:36Z",
-      "content": "Afghanistan has defeated Pakistan in a T20 International (T20I) marking the Afghan sides first win in four T20Is against Pakistan.\r\nThe cricket teams have met seven times in all, playing three previo… [+2859 chars]"
-    },
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
+  articles=  []
   constructor(){
     super();
     console.log("Hello I am a constructor from News Component");
@@ -51,6 +11,16 @@ export class News extends Component {
       
     }
     }
+    async componentDidMount(){
+      console.log("cdm");
+      let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c8349c0396164946b29eac7f1a3144fb"
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData);
+    this.setState({articles:parsedData.articles})
+    
+    }
+
   render() {
     return (
       <div className="container my-4">
@@ -59,7 +29,7 @@ export class News extends Component {
         <div className="row">
             {this.state.articles.map((element)=>{
               return <div className="col md-4" key={element.url}>
-          <NewsItem title={element.title.slice(0,8)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} url={element.url}/>
+          <NewsItem title={element.title?element.title.slice(0,8):" "} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} url={element.url}/>
           </div>
           })}
         </div>
