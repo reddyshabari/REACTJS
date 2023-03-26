c3b1f89c43f13b52981f3e40a7741b7e6c07b849
Shabari <reddyshabari05@gmail.com>
2023-03-26 17:43:27 +0530

    adding previous and next button

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,41 +1,75 @@
 import React, { Component } from 'react'
-import NewsItem from './NewsItem';
+import NewsItem from './NewsItem'
+
 export class News extends Component {
-  articles=  []
-  constructor(){
-    super();
-    console.log("Hello I am a constructor from News Component");
-    this.state={
-      articles:this.articles,
-      loading:false
-      
+
+    constructor(){
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page:1
+        }
     }
+
+    async componentDidMount(){ 
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=271617760c224c41ab65ffc33fe098d7&page=1pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData); 
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
     }
-    async componentDidMount(){
-      console.log("cdm");
-      let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c8349c0396164946b29eac7f1a3144fb"
-    let data=await fetch(url);
-    let parsedData=await data.json();
-    console.log(parsedData);
-    this.setState({articles:parsedData.articles})
+
+     handlePrevClick = async ()=>{
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=271617760c224c41ab65ffc33fe098d7&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);  
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+
+    }
     
+     handleNextClick = async ()=>{
+        console.log("Next"); 
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=271617760c224c41ab65ffc33fe098d7&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);  
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+    }
     }
 
-  render() {
-    return (
-      <div className="container my-4">
-
-        <h2>NewsMonkey - Top headlines</h2>
-        <div className="row">
-            {this.state.articles.map((element)=>{
-              return <div className="col md-4" key={element.url}>
-          <NewsItem title={element.title?element.title.slice(0,8):" "} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} url={element.url}/>
-          </div>
-          })}
-        </div>
-      </div>
-    )
-  }
+    render() { 
+      console.log(this.state.articles);
+        return (
+        
+            <div className="container my-3">
+                <h1>NewsMonkey - Top Headlines</h1> 
+                <div className="row"> 
+                {this.state.articles.map((element)=>{
+                    return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    </div> 
+                })} 
+                </div> 
+                <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-   
     render() {
-        let {title, description, imageUrl, url} = this.props;
+        let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <a href={url} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
@@ -20,4 +19,3 @@ export class NewsItem extends Component {
 }
 
 export default NewsItem
-
